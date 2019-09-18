b613d2ab3f2dd478c5255454dec75dac7a720e3e
Aron Ockerse <32068150+Ockerse@users.noreply.github.com>
2019-09-18 10:04:40 -0400

    Add files via upload

---
+++ CalculatorConversion/CalculatorConversion/UnitsAndModes.swift
@@ -0,0 +1,47 @@
+//
+//  UnitsAndModes.swift
+//  CalculatorConversion
+//
+//  Created by Aron Ockerse on 9/18/19.
+//  Copyright © 2019 Aron Ockerse. All rights reserved.
+//
+
+import Foundation
+
+enum CalculatorMode : String {
+    case Length
+    case Volume
+}
+enum LengthUnit : String, CaseIterable {
+    case Meters = "Meters"
+    case Yards = "Yards"
+    case Miles = "Miles"
+}
+
+enum VolumeUnit : String, CaseIterable {
+    case Liters = "Liters"
+    case Gallons = "Gallons"
+    case Quarts = "Quarts"
+}
+
+struct LengthConversionKey : Hashable {
+    var toUnits : LengthUnit
+    var fromUnits : LengthUnit
+}
+
+struct VolumeConversionKey : Hashable {
+    var toUnits : VolumeUnit
+    var fromUnits : VolumeUnit
+}
+
+let lengthConversionTable : Dictionary<LengthConversionKey, Double> = [
+    LengthConversionKey(toUnits: .Meters, fromUnits: .Meters) : 1.0,
+    LengthConversionKey(toUnits: .Meters, fromUnits: .Yards) : 0.9144,
+    LengthConversionKey(toUnits: .Meters, fromUnits: .Miles) : 1609.34,
+    LengthConversionKey(toUnits: .Yards, fromUnits: .Meters) : 1.09361,
+    LengthConversionKey(toUnits: .Yards, fromUnits: .Yards) : 1.0,
+    LengthConversionKey(toUnits: .Yards, fromUnits: .Miles) : 1760.0,
+    LengthConversionKey(toUnits: .Miles, fromUnits: .Meters) : 0.000621371,
+    LengthConversionKey(toUnits: .Miles, fromUnits: .Yards) : 0.000568182,
+    LengthConversionKey(toUnits: .Miles, fromUnits: .Miles) : 1.0
+]
--- CalculatorConversion/CalculatorConversion/ViewController.swift
+++ CalculatorConversion/CalculatorConversion/ViewController.swift
@@ -12,16 +12,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var fromField: DecimalMinusTextField!
     @IBOutlet weak var toField: DecimalMinusTextField!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
     @IBAction func clearButtonPressed(_ sender: UIButton) {
         toField!.text = ""
         fromField.text = ""
     }
+    
 }
 
