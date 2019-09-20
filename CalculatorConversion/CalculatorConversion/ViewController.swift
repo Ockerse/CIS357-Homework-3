a86ca150f38dbcdac12a413e77dc15ba6b7275c2
Aron <ockersar@mail.gvsu.edu>
2019-09-20 13:01:16 -0400

    added more variables

--- CalculatorConversion/CalculatorConversion/ViewController.swift
+++ CalculatorConversion/CalculatorConversion/ViewController.swift
@@ -11,10 +11,27 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var fromField: DecimalMinusTextField!
     @IBOutlet weak var toField: DecimalMinusTextField!
-    @IBOutlet weak var label1: UILabel!
-    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var fromLabel: UILabel!
+    @IBOutlet weak var toLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var prefix = "Enter length in "
+    var unit = CalculatorMode.Length
+    var tolength = LengthUnit.Yards
+    var fromlength = LengthUnit.Yards
+    
+    var tovolume = VolumeUnit.Gallons
+    var fromvolume = VolumeUnit.Gallons
     
     
+    var mode : CalculatorMode?
+
+    func unitsSelection(from: String, to: String) {
+        self.fromLabel!.text = from
+        self.fromField!.placeholder = prefix + fromLabel!.text!
+        self.toLabel!.text = to
+        self.toField!.placeholder = prefix + toLabel!.text!
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,7 +55,45 @@ class ViewController: UIViewController {
         else if self.fromField.text == "" && self.toField.text != ""{
             
         }
+        
+        
     }
+    
+    func currentMode(){
+        switch fromLabel.text{
+        case "Yards":
+            fromlength = .Yards
+        case "Meters":
+            fromlength = .Meters
+        case "Miles":
+            fromlength = .Miles
+        case "Gallons":
+            fromvolume = .Gallons
+        case "Liters":
+            fromvolume = .Liters
+        case "Quarts":
+            fromvolume = .Quarts
+        default:
+            print("There is an error")
+        }
+        
+        switch toLabel.text{
+        case "Yards":
+            tolength = .Yards
+        case "Meters":
+            tolength = .Meters
+        case "Miles":
+            tolength = .Miles
+        case "Gallons":
+            tovolume = .Gallons
+        case "Liters":
+            tovolume = .Liters
+        case "Quarts":
+            tovolume = .Quarts
+        default:
+            print("There is an error")
+        }
 }
 
 
+}
